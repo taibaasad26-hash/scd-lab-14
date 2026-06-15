b232365f1c48d573425781c5be984bcf7450ca50
taibaasad26-hash <taibaasad26@gmail.com>
2026-06-15 09:25:31 +0500

    Add BankAccount class with deposit and balance methods

---
+++ bankaccount.java
@@ -0,0 +1,16 @@
+public class BankAccount {
+
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
